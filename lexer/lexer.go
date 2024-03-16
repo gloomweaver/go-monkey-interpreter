53de9591e1f58e1bcc734fda0b0a77e480f4394f
Kirill Khramkov <hramkir@gmail.com>
2024-03-16 13:02:33 +0400

    Correctly parse identifiers and values

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -27,33 +27,74 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
-func mapToken(ch byte) token.Token {
-	switch ch {
-	case '=':
-		return token.Token{Type: token.ASSIGN, Literal: string(ch)}
-	case ';':
-		return token.Token{Type: token.SEMICOLON, Literal: string(ch)}
-	case '(':
-		return token.Token{Type: token.LPAREN, Literal: string(ch)}
-	case ')':
-		return token.Token{Type: token.RPAREN, Literal: string(ch)}
-	case ',':
-		return token.Token{Type: token.COMMA, Literal: string(ch)}
-	case '+':
-		return token.Token{Type: token.PLUS, Literal: string(ch)}
-	case '{':
-		return token.Token{Type: token.LBRACE, Literal: string(ch)}
-	case '}':
-		return token.Token{Type: token.RBRACE, Literal: string(ch)}
-	case 0:
-		return token.Token{Type: token.EOF, Literal: ""}
-	default:
-		return token.Token{Type: token.ILLEGAL, Literal: string(ch)}
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
 	}
 }
 
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) NextToken() token.Token {
-	tok := mapToken(l.ch)
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = token.Token{Type: token.ASSIGN, Literal: string(l.ch)}
+	case ';':
+		tok = token.Token{Type: token.SEMICOLON, Literal: string(l.ch)}
+	case '(':
+		tok = token.Token{Type: token.LPAREN, Literal: string(l.ch)}
+	case ')':
+		tok = token.Token{Type: token.RPAREN, Literal: string(l.ch)}
+	case ',':
+		tok = token.Token{Type: token.COMMA, Literal: string(l.ch)}
+	case '+':
+		tok = token.Token{Type: token.PLUS, Literal: string(l.ch)}
+	case '{':
+		tok = token.Token{Type: token.LBRACE, Literal: string(l.ch)}
+	case '}':
+		tok = token.Token{Type: token.RBRACE, Literal: string(l.ch)}
+	case 0:
+		tok = token.Token{Type: token.EOF, Literal: ""}
+	default:
+		if isLetter(l.ch) {
+			ident := l.readIdentifier()
+			identType := token.LookupIdent(ident)
+			return token.Token{Type: identType, Literal: ident}
+		} else if isDigit(l.ch) {
+			ident := l.readNumber()
+			return token.Token{Type: token.INT, Literal: ident}
+		} else {
+			tok = token.Token{Type: token.ILLEGAL, Literal: string(l.ch)}
+		}
+	}
+
 	l.readChar()
+
 	return tok
 }
